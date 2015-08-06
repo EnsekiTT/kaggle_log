5a0e663c0744c1da342c16934ba2bd520cc0fb00
EnsekiTT <ensekitt@curbsci.com>
2015-08-06 22:14:35 +0900

    try deep learning

--- DigitRecognizer/src/digitrecognizer.py
+++ DigitRecognizer/src/digitrecognizer.py
@@ -5,7 +5,9 @@ from sklearn import cross_validation
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
+from keras.utils import np_utils
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.core import Dense, Activation, Dropout, Flatten
 
 class DigitRecognizer():
     def __init__(self):
@@ -18,9 +20,17 @@ class DigitRecognizer():
 
     def preprocessing(self):
         # 前処理
-        self.training_datas = self.training.ix[:, 1:].values.copy()
-        self.training_labels = self.training.ix[:, 0].values.copy()
-        self.testing_datas = self.testing.ix[:, 0:].values.copy()
+        self.nb_classes = 10
+
+        t_datas = self.training.ix[:, 1:].values.copy()
+        self.training_datas = t_datas.reshape(t_datas.shape[0], 1, 28, 28)
+
+        t_labels = self.training.ix[:, 0].values.copy()
+        self.training_labels = np_utils.to_categorical(t_labels, self.nb_classes)
+
+        t_datas = self.testing.ix[:, 0:].values.copy()
+        self.testing_datas = t_datas.reshape(t_datas.shape[0], 1, 28, 28)
+
         self.testing_labels = None
 
     def model_tuning(self):
@@ -33,12 +43,28 @@ class DigitRecognizer():
         # Deep learning
         dims = self.training_datas.shape[1]
         print(dims, 'dims')
+
         self.model = Sequential()
-        self.model.add(Dense(input_dim=dims, output_dim=64, init="glorot_uniform"))
-        self.model.add(Activation("relu"))
-        self.model.add(Dense(input_dim=64, output_dim=10, init="glorot_uniform"))
-        self.model.add(Activation("softmax"))
-        self.model.compile(loss='categorical_crossentropy', optimizer='sgd')
+
+        self.model.add(Convolution2D(32, 1, 3, 3, border_mode='full'))
+        self.model.add(Activation('relu'))
+        self.model.add(Convolution2D(32, 32, 3, 3))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(poolsize=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(32*196, 128))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+
+        self.model.add(Dense(128, 32))
+        self.model.add(Activation('relu'))
+
+        self.model.add(Dense(32, self.nb_classes))
+        self.model.add(Activation('softmax'))
+
+        self.model.compile(loss='categorical_crossentropy', optimizer='adadelta')
 
     def cross_validation(self):
         scores = cross_validation.cross_val_score(
@@ -51,10 +77,8 @@ class DigitRecognizer():
         # self.model.fit(self.training_datas, self.training_labels)
 
         # DeepLearning
-        #training_datas = [self.training_datas.ix[i].values.tolist() for i in range(len(self.training_datas))]
-        #training_labels = [self.training_labels.ix[i] for i in range(len(self.training_labels))]
-        self.model.fit(self.training_datas, self.training_labels,
-            verbose=1, nb_epoch=2, batch_size=1, validation_split=0.15)
+        self.model.fit(self.training_datas, self.training_labels, show_accuracy=True,
+            verbose=1, nb_epoch=12, batch_size=64, validation_split=0.15)
 
     def predicting(self):
         # 評価
@@ -62,8 +86,7 @@ class DigitRecognizer():
         #self.answer = self.model.predict(self.testing_datas)
 
         # DeepLearning
-        #testing_datas = [self.testing_datas.ix[i].values.tolist() for i in range(len(self.testing_datas))]
-        self.answer = model.predict_classes(self.testing_datas)
+        self.answer = self.model.predict_classes(self.testing_datas)
 
     def writing(self):
         # 書き出し
